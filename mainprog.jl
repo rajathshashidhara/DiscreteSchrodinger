f9eb430b6b9a43f9f429a90ed1aa5cd52c6216eb
Rajath Shashidhara <rajaths.rajaths@gmail.com>
2015-06-30 17:05:06 +0800

    Dynamic Time step adjustment added

--- mainprog.jl
+++ mainprog.jl
@@ -34,29 +34,92 @@ function kineticenergy{T<:Real}(px::T, py::T, pz::T)
 end
 
 function solveschrodingereqn()
-    pdesc = ProblemDescription(0.1, [4.0], [Int16(64)], 0.4, Int8(4))
+    pdesc = ProblemDescription(0.5, [4.0], [Int16(64)], 0.05, Int8(4))
     sysdesc = generatesystemdescriptor(pdesc)
 
-    s = StateSet(Complex128, 4, 64)
+    ψ = StateSet(Complex128, 4, 64)
 
     T = discretizeOperators(kineticenergy, sysdesc, pdesc, false)
     V = discretizeOperators(harmonicpotential, sysdesc, pdesc, true)
 
+    # convergence constants
+    ϵ = pdesc.timestep
+    χ = 0.5
+    γ = pdesc.tolerence
+
     # second order factorization
-    eV = exp(-0.5*pdesc.timestep*V)
-    eT = exp(-1.0*pdesc.timestep*T)
+    eV = exp(-0.5*ϵ*V)
+    eVˈ = exp(-0.5*ϵ*χ*V)
+    eT = exp(-1.0*ϵ*T)
+    eTˈ = exp(-1.0*ϵ*χ*T)
     τ = eV*eT*eV
+    τˈ = eVˈ*eTˈ*eVˈ
+
+    multiply!(τ, ψ, sysdesc)
+    sysdesc.normenergies = log(orthonormalize!(ψ))/(-2.0*ϵ)
+
+    H = expectationvalue(ψ, V)
+    momentumspace!(ψ, sysdesc)
+    H += expectationvalue(ψ, T)
+    positionspace!(ψ, sysdesc)
+    δH = 0.0
+    δE = 0.0
 
     for i=1:100
-        multiply!(τ, s, sysdesc)
-        orthonormalize!(s)
+        ψˈ = deepcopy(ψ)
+
+        multiply!(τ, ψ, sysdesc)
+        E = log(orthonormalize!(ψ))/(-2.0*ϵ)
+        rmsE = norm(E)/sqrt(pdesc.nstates)
+        δE = norm(E - sysdesc.normenergies)/sqrt(pdesc.nstates)
+
+        multiply!(τˈ,ψˈ, sysdesc)
+        Eˈ = log(orthonormalize!(ψˈ))/(-2.0*ϵ*χ)
+        rmsEˈ = norm(Eˈ)/sqrt(pdesc.nstates)
+        δEˈ = norm(Eˈ - sysdesc.normenergies)/sqrt(pdesc.nstates)
+
+        H = expectationvalue(ψ, V)
+        momentumspace!(ψ, sysdesc)
+        H += expectationvalue(ψ, T)
+        positionspace!(ψ, sysdesc)
+        δH = norm(E - H)/sqrt(pdesc.nstates)
+
+        Hˈ = expectationvalue(ψˈ, V)
+        momentumspace!(ψˈ, sysdesc)
+        Hˈ += expectationvalue(ψˈ, T)
+        positionspace!(ψˈ, sysdesc)
+        δHˈ = norm(Eˈ - Hˈ)/sqrt(pdesc.nstates)
+
+        if rmsE<rmsEˈ || δE<δEˈ
+            χ = sqrt(χ)
+            sysdesc.normenergies = E
+            sysdesc.energies = H
+        else
+            ϵ *= χ
+            χ = χ^2
+            ψ = ψˈ
+            sysdesc.normenergies = Eˈ
+            sysdesc.energies = Hˈ
+            δE = δEˈ
+            δH = δHˈ
+        end
+
+        if δE <= γ*δH
+            @show i
+            break
+        end
+
+        eV = exp(-0.5*ϵ*V)
+        eVˈ = exp(-0.5*ϵ*χ*V)
+        eT = exp(-1.0*ϵ*T)
+        eTˈ = exp(-1.0*ϵ*χ*T)
+        τ = eV*eT*eV
+        τˈ = eVˈ*eTˈ*eVˈ
     end
 
-    energy = expectationvalue(s, V)
-    momentumspace!(s, sysdesc)
-    energy += expectationvalue(s, T)
-    positionspace!(s, sysdesc)
-    abs(energy)
+    @show abs(H)
+    @show δE
+    @show δH
 end
 
-@show solveschrodingereqn()
+@time solveschrodingereqn()
